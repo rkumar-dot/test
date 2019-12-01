dd73c2a34dce6677511f5bfec88311d92c2b8c59
rkumar-dot <58239123+rkumar-dot@users.noreply.github.com>
2019-12-01 13:18:50 -0800

    Create main.tf

---
+++ waf/main.tf
@@ -0,0 +1,88 @@
+# Random ID Generator
+
+resource "random_id" "this" {
+  count = "${lower(var.target_scope) == "regional" || lower(var.target_scope) == "global" ? "1" : "0"}"
+
+  byte_length = "8"
+
+  keepers = {
+    target_scope = "${lower(var.target_scope)}"
+  }
+}
+
+# Regional
+
+## OWASP Top 10 A1
+### Mitigate SQL Injection Attacks
+### Matches attempted SQLi patterns in the URI, QUERY_STRING, BODY, COOKIES
+resource "aws_wafregional_sql_injection_match_set" "owasp_01_sql_injection_set" {
+  count = "${lower(var.target_scope) == "regional" ? "1" : "0"}"
+
+  name = "${lower(var.service_name)}-owasp-01-detect-sql-injection-${random_id.this.0.hex}"
+
+  sql_injection_match_tuple {
+    text_transformation = "URL_DECODE"
+
+    field_to_match {
+      type = "URI"
+    }
+  }
+
+  sql_injection_match_tuple {
+    text_transformation = "HTML_ENTITY_DECODE"
+
+    field_to_match {
+      type = "URI"
+    }
+  }
+
+  sql_injection_match_tuple {
+    text_transformation = "URL_DECODE"
+
+    field_to_match {
+      type = "QUERY_STRING"
+    }
+  }
+
+  sql_injection_match_tuple {
+    text_transformation = "HTML_ENTITY_DECODE"
+
+    field_to_match {
+      type = "QUERY_STRING"
+    }
+  }
+
+  sql_injection_match_tuple {
+    text_transformation = "URL_DECODE"
+
+    field_to_match {
+      type = "BODY"
+    }
+  }
+
+  sql_injection_match_tuple {
+    text_transformation = "HTML_ENTITY_DECODE"
+
+    field_to_match {
+      type = "BODY"
+    }
+  }
+
+  sql_injection_match_tuple {
+    text_transformation = "URL_DECODE"
+
+    field_to_match {
+      type = "HEADER"
+      data = "Authorization"
+    }
+  }
+
+  sql_injection_match_tuple {
+    text_transformation = "HTML_ENTITY_DECODE"
+
+    field_to_match {
+      type = "HEADER"
+      data = "Authorization"
+    }
+  }
+}
