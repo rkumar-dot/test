ffcd2b12ba4b395b30157eaf1a2cebb59afd28ad
rkumar-dot <58239123+rkumar-dot@users.noreply.github.com>
2019-12-03 21:42:25 -0800

    Update outputs.tf

--- waf1/outputs.tf
+++ waf1/outputs.tf
@@ -1,4 +1,4 @@
-output "07_size_restriction_rule_id" {
+output "size_restriction_rule_id" {
   description = "AWS WAF Rule which mitigates abnormal requests via size restrictions."
   value       = "Test1"
 }
